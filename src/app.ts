af7f46f86ff4353ed8fc2728da7f9bbba7d19e87
Poseidon989 Poseidon989 <enzo.haegel@epitech.eu>
2023-03-27 21:19:11 +0800

    Refacto clean code

--- src/app.ts
+++ src/app.ts
@@ -1,148 +1,184 @@
-// Get the content element
-const content = document.getElementById('content') || document.body;
+class CofScheduleExtension {
+    private readonly contentElement: HTMLElement;
 
-// Define a type for the result of the chrome.tabs.query function
-interface TabQueryResult {
-    id: number;
-    url: string;
-    title: string;
-    // You can add more properties here if needed
-}
+    constructor() {
+        this.contentElement =
+            document.getElementById('content') || document.body;
+        this.setupTabListener();
+        this.setupLocalStorage();
+    }
 
-interface Class {
-    name: string;
-    teacher: string;
-    classroom: string;
-    time: string;
-    duration: number;
-}
+    private setupTabListener(): void {
+        chrome.tabs.query(
+            {currentWindow: true, active: true},
+            (tabs: chrome.tabs.Tab[]) => {
+                const tabQueryResults: TabQueryResult[] = tabs.map(tab => ({
+                    id: tab.id!,
+                    url: tab.url!,
+                    title: tab.title!,
+                }));
 
-// Call chrome.tabs.query to get information about the current tab
-chrome.tabs.query(
-    {currentWindow: true, active: true},
-    (tabs: chrome.tabs.Tab[]) => {
-        // Convert the chrome.tabs.Tab[] array to TabQueryResult[] array
-        const tabQueryResults: TabQueryResult[] = tabs.map(tab => ({
-            id: tab.id!,
-            url: tab.url!,
-            title: tab.title!,
-            // You can add more properties here if needed
-        }));
+                const activeTab = tabQueryResults[0];
+                const activeUrl = activeTab.url;
 
-        // We only requested information for the active tab, so the array will have a single element
-        const activeTab = tabQueryResults[0];
-        const activeUrl = activeTab.url;
+                if (activeUrl.includes('cof.ntpu.edu.tw')) {
+                    this.setupScheduleButton(activeTab);
+                } else {
+                    this.setupNonCofSiteMessage();
+                }
+            }
+        );
+    }
 
-        // Do something with the active URL, e.g. log it to the console
-        if (activeUrl.includes('cof.ntpu.edu.tw')) {
-            // Create a button element and append it to the content element
+    private setupScheduleButton(activeTab: TabQueryResult): void {
+        const button = document.createElement('button');
+        button.textContent = 'Import Schedule';
+        this.contentElement.appendChild(button);
+
+        button.addEventListener('click', () => {
+            chrome.scripting
+                .executeScript({
+                    target: {tabId: activeTab.id},
+                    func: getPageSource,
+                })
+                .then(injectionResults => {
+                    console.log('injectionResults: ', injectionResults);
+                    const sourceCode = injectionResults[0].result;
+                    // the result is html code table of the schedule, add it to the current extension page
+                    const parser = new DOMParser();
+                    const doc = parser.parseFromString(sourceCode, 'text/html');
+                    const table = doc.getElementsByTagName('table')[0];
+
+                    // if a child table exist remove it
+                    if (this.contentElement.getElementsByTagName('table')[0]) {
+                        this.contentElement.removeChild(
+                            this.contentElement.getElementsByTagName('table')[0]
+                        );
+                    }
+
+                    localStorage.setItem('table_schedule', sourceCode);
+                    this.setupLocalStorage();
+
+                    // const courses = this.parseSchedule(String(table));
+                    // console.log(courses);
+                    // this.showSchedule(courses);
+                });
+        });
+    }
+
+    private setupNonCofSiteMessage(): void {
+        const message = document.createElement('div');
+        message.id = 'message';
+        message.textContent = 'You are not on cof.ntpu.edu.tw';
+        this.contentElement.appendChild(message);
+
+        const link = document.createElement('a');
+        link.href = 'https://cof.ntpu.edu.tw';
+        link.target = '_blank';
+        link.textContent = 'Go to Website';
+
+        const button = document.createElement('button');
+        button.classList.add('button-cof');
+        button.appendChild(link);
+
+        this.contentElement.appendChild(button);
+    }
+
+    private setupLocalStorage(): void {
+        const tableSchedule = localStorage.getItem('table_schedule');
+
+        if (tableSchedule) {
+            const parser = new DOMParser();
+            const doc = parser.parseFromString(tableSchedule, 'text/html');
+            const table = doc.getElementsByTagName('table')[0];
+            this.contentElement.appendChild(table);
+
+            // check if a button with button_clear_storage as id exist
+            if (document.getElementById('button_clear_storage')) {
+                return;
+            }
+
+            // add a button to clear the local storage
             const button = document.createElement('button');
-            button.textContent = 'Button Text';
-            content.appendChild(button);
-            // call the function getPageSource when the button is clicked
+            button.id = 'button_clear_storage';
+            button.textContent = 'Clear Local Storage';
+            this.contentElement.appendChild(button);
+
             button.addEventListener('click', () => {
-                chrome.scripting
-                    .executeScript({
-                        target: {tabId: activeTab.id},
-                        func: getPageSource,
-                    })
-                    .then(injectionResults => {
-                        console.log('injectionResults: ', injectionResults);
-                        const sourceCode = injectionResults[0].result;
-                        // the result is html code table of the schedule, add it to the current extension page, and remove the button
-                        const parser = new DOMParser();
-                        const doc = parser.parseFromString(sourceCode, 'text/html');
-                        const table = doc.getElementsByTagName('table')[0];
-                        // if a child table exist remove it
-                        if (content.getElementsByTagName('table')[0]) {
-                            content.removeChild(content.getElementsByTagName('table')[0]);
-                        }
-                        content.appendChild(table);
-                        // add the table schedule to the local storage for the next time
-                        localStorage.setItem('table_schedule', sourceCode);
-                        content.removeChild(button);
-                        // get the class list
-                        const classList = getClass(sourceCode);
-                        console.log(classList);
-                        // create a new button to show the class list
-                        const button2 = document.createElement('button');
-                        button2.textContent = 'Show Class List';
-                        content.appendChild(button2);
-                        // add event listener to the button
-                        button2.addEventListener('click', () => {
-                            // create a new div to show the class list
-                            const classListDiv = document.createElement('div');
-                            classListDiv.id = 'classListDiv';
-                            // add the class list to the div
-                            classList.forEach((classInfo) => {
-                                const classDiv = document.createElement('div');
-                                classDiv.textContent = classInfo;
-                                classListDiv.appendChild(classDiv);
-                            });
-                            // add the div to the content element
-                            content.appendChild(classListDiv);
-                            // remove the button
-                            content.removeChild(button2);
-                        });
-                    });
+                localStorage.clear();
+                this.contentElement.removeChild(button);
+                this.contentElement.removeChild(
+                    this.contentElement.getElementsByTagName('table')[0]
+                );
             });
-        } else {
-            // Create a message element and append it to the content element
-            const message = document.createElement('div');
-            message.id = 'message';
-            message.textContent = 'You are not on cof.ntpu.edu.tw';
-            content.appendChild(message);
-
-            // Create a link element in a button and append it to the content element
-            const link = document.createElement('a');
-            link.href = 'https://cof.ntpu.edu.tw';
-            link.target = '_blank';
-            link.textContent = 'Go to Website';
-            const button = document.createElement('button');
-            // add class button-cof to the button
-            button.classList.add('button-cof');
-            button.appendChild(link);
-            content.appendChild(button);
         }
     }
-);
+
+    private parseSchedule(html: string): Course[] {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(html, 'text/html');
+        const rows = Array.from(doc.querySelectorAll('tr'));
+        const headerCols = Array.from(rows[0].querySelectorAll('th'));
+        const days = headerCols.slice(1).map(col => col.textContent?.trim());
+        const courses: Course[] = [];
+
+        for (let i = 1; i < rows.length; i++) {
+            const cols = Array.from(rows[i].querySelectorAll('td'));
+            const time = cols[0]?.textContent?.trim();
+            for (let j = 1; j < cols.length; j++) {
+                const courseInfo = cols[j].innerHTML.trim().split('<br>');
+                if (courseInfo.length >= 3) {
+                    const name = courseInfo[0];
+                    const location = courseInfo[2];
+                    const course = {
+                        name,
+                        location,
+                        day: days[j - 1] || '',
+                        time: time || '',
+                    };
+                    courses.push(course);
+                }
+            }
+        }
+
+        return courses;
+    }
+
+    private showSchedule(courses: Course[]): void {
+        // create a cool dark theme schedule table with material ui
+        const table = document.createElement('table');
+        const headerRow = document.createElement('tr');
+        const headerCols = ['Name', 'Location', 'Day', 'Time'];
+        for (const headerCol of headerCols) {
+            const col = document.createElement('th');
+            col.textContent = headerCol;
+            headerRow.appendChild(col);
+        }
+        table.appendChild(headerRow);
+
+        for (const course of courses) {
+            const row = document.createElement('tr');
+            const cols = [course.name, course.location, course.day, course.time];
+            for (const col of cols) {
+                const colElement = document.createElement('td');
+                colElement.textContent = col;
+                row.appendChild(colElement);
+            }
+            table.appendChild(row);
+        }
+        this.contentElement.appendChild(table);
+    }
+}
 
 // Function that return source code
 function getPageSource(): string {
     const mainFrame: any = document.querySelector('frame[name="mainFrame"]');
     if (mainFrame) {
-        return mainFrame.contentDocument.documentElement.innerHTML
+        return mainFrame.contentDocument.documentElement.innerHTML;
     } else {
         console.error('Frame with name "mainFrame" not found.');
-        return "null";
+        return 'null';
     }
 }
 
-function getClass(sourceCode: string): string[] {
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(sourceCode, 'text/html');
-    const classList = doc.getElementsByClassName('class');
-    const classArray: string[] = [];
-    for (let i = 0; i < classList.length; i++) {
-        const classElement = classList[i] as HTMLElement;
-        const classInfo = classElement.innerText;
-        classArray.push(classInfo);
-    }
-    return classArray;
-}
-
-// // Create a logo element and append it to the content element
-// const logo = document.createElement('img');
-// logo.id = 'logo';
-// logo.src = 'assets/fulllogo_nobuffer.png';
-// content.prepend(logo);
-
-// check if the table schedule is in the local storage
-const tableSchedule = localStorage.getItem('table_schedule');
-if (tableSchedule) {
-    // if it is, add it to the current extension page
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(tableSchedule, 'text/html');
-    const table = doc.getElementsByTagName('table')[0];
-    content.appendChild(table);
-}
+const cofScheduleExtension = new CofScheduleExtension();
---
+++ src/models.ts
@@ -0,0 +1,12 @@
+interface TabQueryResult {
+    id: number;
+    url: string;
+    title: string;
+}
+
+interface Course {
+    name: string;
+    location: string;
+    day: string;
+    time: string;
+}
